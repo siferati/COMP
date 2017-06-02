cb7efe84eecc8da04bcb26becf9fef9f8415896a
tirafesi <tirafesi@hotmail.com>
2017-06-02 15:59:54 +0100

    updating stuff

--- GSON/GsonTest.java
+++ GSON/GsonTest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2017 SPeCS.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -41,18 +41,18 @@ public class GsonTest {
         File json = new File("ast.json");
 
         Root javaRootMapObject = gson.fromJson(read(json), Root.class);
-		
+
 		System.out.println(javaRootMapObject);
-		
-		
+
+
     }
 
     /**
      * Given a File object, returns a String with the contents of the file.
-     * 
+     *
      * <p>
      * If an error occurs (ex.: the File argument does not represent a file) returns null and logs the cause.
-     * 
+     *
      * @param file
      *            a File object representing a file.
      * @return a String with the contents of the file.
